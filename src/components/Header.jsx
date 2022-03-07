d1b48c0e4eea3af8716b25789b600a914fd16b7d
moraes <moraeslua66@gmail.com>
2022-03-07 19:30:40 -0300

    dev: desevolvendo calculo total de despesas

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,23 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 class Header extends Component {
+  sumTotalAmountOfExpenses(expenses = []) {
+    const expensesAmount = expenses.reduce((acc, { currency, exchangeRates, value }) => {
+      console.log(currency);
+      const currencyConvertedToBRL = exchangeRates[currency].ask;
+      console.log(currencyConvertedToBRL);
+      const expenseValueInBRL = value * currencyConvertedToBRL;
+      console.log(expenseValueInBRL);
+      return acc + expenseValueInBRL;
+    }, 0);
+    return expensesAmount;
+  }
+
   render() {
-    const { userEmail } = this.props;
+    const { userEmail, expenses } = this.props;
+
+    console.log(this.sumTotalAmountOfExpenses(expenses));
+
     return (
       <header>
         <p data-testid="email-field">
@@ -12,9 +27,8 @@ class Header extends Component {
         </p>
 
         <p data-testid="total-field">
-          0
+          {expenses.length > 0 ? parseInt(this.sumTotalAmountOfExpenses(), 10) : '0,00'}
         </p>
-
         <p data-testid="header-currency-field">
           BRL
         </p>
@@ -25,10 +39,12 @@ class Header extends Component {
 
 const mapStateToProps = (state) => ({
   userEmail: state.user.email,
+  expenses: state.wallet.expenses,
 });
 
 Header.propTypes = {
   userEmail: PropTypes.string.isRequired,
+  expenses: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default connect(mapStateToProps, null)(Header);
