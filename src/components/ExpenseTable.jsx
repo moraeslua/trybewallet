8a442e92d841ac69278f38c698e9df7489e4f2fc
moraes <moraeslua66@gmail.com>
2022-03-07 23:28:00 -0300

    fix: deleta ExpenseTable.css

--- src/components/ExpenseTable.jsx
+++ src/components/ExpenseTable.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import './ExpenseTable.css';
 import PropTypes from 'prop-types';
 
 class ExpenseTable extends Component {
